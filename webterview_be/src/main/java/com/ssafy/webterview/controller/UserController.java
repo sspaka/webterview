1f01f605a0b2122fbeff70bc60f9a386ab302f27
jyoungl <50093993+jyoungl@users.noreply.github.com>
2022-07-26 18:35:33 +0900

    #18 feat 회원가입 로그인 연결

--- webterview_be/src/main/java/com/ssafy/webterview/controller/UserController.java
+++ webterview_be/src/main/java/com/ssafy/webterview/controller/UserController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.webterview.service.JwtServiceImpl;
 import com.ssafy.webterview.service.MailService;
@@ -21,6 +18,8 @@ import com.ssafy.webterview.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api("UserController V1")
 @RestController
@@ -40,61 +39,61 @@ public class UserController {
 	@Autowired
 	private MailService mailService;
 
-//	@ApiOperation(value = "로그인", notes = "Access-token과 로그인 결과 메세지를 반환한다.", response = Map.class)
-//	@PostMapping("/login")
-//	public ResponseEntity<Map<String, Object>> login(
-//			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) User userDto) {
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = null;
-//		try {
-//			User loginUser = userService.login(userDto);
-//			if (loginUser != null) {
-//				String token = jwtService.create("useremail", loginUser.getUserEmail(), "access-token");// key, data, subject
-//				logger.debug("로그인 토큰정보 : {}", token);
-//				resultMap.put("access-token", token);
-//				resultMap.put("message", SUCCESS);
-//				status = HttpStatus.ACCEPTED;
-//			} else {
-//				resultMap.put("message", FAIL);
-//				status = HttpStatus.ACCEPTED;
-//			}
-//		} catch (Exception e) {
-//			logger.error("로그인 실패 : {}", e);
-//			resultMap.put("message", e.getMessage());
-//			status = HttpStatus.INTERNAL_SERVER_ERROR;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
-//
-//	@ApiOperation(value = "회원 정보 보기", notes = "회원 정보를 담은 Token을 반환한다.", response = Map.class)
-//	@GetMapping("/info/{useremail}")
-//	public ResponseEntity<Map<String, Object>> getInfo(
-//			@PathVariable("useremail") @ApiParam(value = "인증할 회원의 이메일.", required = true) String useremail,
-//			HttpServletRequest request) {
-////		logger.debug("userid : {} ", userid);
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		if (jwtService.isUsable(request.getHeader("access-token"))) {
-//			logger.info("사용 가능한 토큰!!!");
-//			try {
-////				로그인 사용자 정보.
-//				logger.info(useremail);
-//				User userDto = userService.userInfo(useremail);
-//				resultMap.put("userInfo", userDto);
-//				resultMap.put("message", SUCCESS);
-//				status = HttpStatus.ACCEPTED;
-//			} catch (Exception e) {
-//				logger.error("정보조회 실패 : {}", e);
-//				resultMap.put("message", e.getMessage());
-//				status = HttpStatus.INTERNAL_SERVER_ERROR;
-//			}
-//		} else {
-//			logger.error("사용 불가능 토큰!!!");
-//			resultMap.put("message", FAIL);
-//			status = HttpStatus.ACCEPTED;
-//		}
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
+	@ApiOperation(value = "로그인", notes = "Access-token과 로그인 결과 메세지를 반환한다.", response = Map.class)
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, Object>> login(
+			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) UserDto userDto) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			UserDto loginUser = userService.login(userDto);
+			if (loginUser != null) {
+				String token = jwtService.create("useremail", loginUser.getUserEmail(), "access-token");// key, data, subject
+				logger.debug("로그인 토큰정보 : {}", token);
+				resultMap.put("access-token", token);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} else {
+				resultMap.put("message", FAIL);
+				status = HttpStatus.ACCEPTED;
+			}
+		} catch (Exception e) {
+			logger.error("로그인 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+
+	@ApiOperation(value = "회원 정보 보기", notes = "회원 정보를 담은 Token을 반환한다.", response = Map.class)
+	@GetMapping("/info/{useremail}")
+	public ResponseEntity<Map<String, Object>> getInfo(
+			@PathVariable("useremail") @ApiParam(value = "인증할 회원의 이메일.", required = true) String useremail,
+			HttpServletRequest request) {
+//		logger.debug("userid : {} ", userid);
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		if (jwtService.isUsable(request.getHeader("access-token"))) {
+			logger.info("사용 가능한 토큰!!!");
+			try {
+//				로그인 사용자 정보.
+				logger.info(useremail);
+				UserDto userDto = userService.userInfo(useremail);
+				resultMap.put("userInfo", userDto);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} catch (Exception e) {
+				logger.error("정보조회 실패 : {}", e);
+				resultMap.put("message", e.getMessage());
+				status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+		} else {
+			logger.error("사용 불가능 토큰!!!");
+			resultMap.put("message", FAIL);
+			status = HttpStatus.ACCEPTED;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
 //
 //	@ApiOperation(value = "이메일 중복확인", notes="이메일 중복확인 후 성공여부를 반환한다.", response = Map.class)
 //	@PostMapping("/overlap")
@@ -150,11 +149,11 @@ public class UserController {
 
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-//
-//	// 회원정보수정//modify
+
+	// 회원정보수정//modify
 //	@ApiOperation(value = "회원 정보 수정", notes = "회원 정보 수정 후 성공여부와 User 객체를 반환한다. ", response = Map.class)
 //	@PutMapping("/modify")
-//	public ResponseEntity<Map<String, Object>> modify(@RequestBody @ApiParam(value="수정할 정보를 담은 User객체", required=true) User userDto, HttpServletRequest request)
+//	public ResponseEntity<Map<String, Object>> modify(@RequestBody @ApiParam(value="수정할 정보를 담은 User객체", required=true) UserDto userDto, HttpServletRequest request)
 //			throws Exception {
 //		Map<String, Object> resultMap = new HashMap<>();
 //		HttpStatus status = HttpStatus.ACCEPTED;
@@ -162,8 +161,7 @@ public class UserController {
 //		if (jwtService.isUsable(request.getHeader("access-token"))) {
 //			logger.info("사용 가능한 토큰!!!");
 //			try {
-//				userService.modify(userDto);
-//				resultMap.put("userInfo", userService.userInfo(userDto.getUserEmail()));
+//				resultMap.put("userInfo", userService.modify(userDto));
 //				resultMap.put("message", SUCCESS);
 //				status = HttpStatus.ACCEPTED;
 //			} catch (Exception e) {
@@ -179,33 +177,33 @@ public class UserController {
 //
 //		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 //	}
-//
-//	// 회원탈퇴//delete
-//	@ApiOperation(value = "회원 탈퇴", notes = "회원탈퇴 후 성공여부를 반환한다.", response = Map.class)
-//	@DeleteMapping("/delete/{useremail}")
-//	public ResponseEntity<Map<String, Object>> delete(@PathVariable("useremail")  @ApiParam(value="탈퇴할 회원 이메일", required=true) String useremail, HttpServletRequest request)
-//			throws Exception {
-//		Map<String, Object> resultMap = new HashMap<>();
-//		HttpStatus status = HttpStatus.ACCEPTED;
-//		if (jwtService.isUsable(request.getHeader("access-token"))) {
-//			logger.info("사용 가능한 토큰!!!");
-//			try {
-//				userService.delete(useremail);
-//				resultMap.put("message", SUCCESS);
-//				status = HttpStatus.ACCEPTED;
-//			} catch (Exception e) {
-//				logger.error("회원탈퇴 실패 : {}", e);
-//				resultMap.put("message", e.getMessage());
-//				status = HttpStatus.INTERNAL_SERVER_ERROR;
-//			}
-//		} else {
-//			logger.error("사용 불가능 토큰!!!");
-//			resultMap.put("message", FAIL);
-//			status = HttpStatus.ACCEPTED;
-//		}
-//
-//		return new ResponseEntity<Map<String, Object>>(resultMap, status);
-//	}
+
+	 //delete
+	@ApiOperation(value = "회원 탈퇴", notes = "회원탈퇴 후 성공여부를 반환한다.", response = Map.class)
+	@DeleteMapping("/delete/{useremail}")
+	public ResponseEntity<Map<String, Object>> delete(@PathVariable("useremail")  @ApiParam(value="탈퇴할 회원 이메일", required=true) String useremail, HttpServletRequest request)
+			throws Exception {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		if (jwtService.isUsable(request.getHeader("access-token"))) {
+			logger.info("사용 가능한 토큰!!!");
+			try {
+				userService.delete(useremail);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} catch (Exception e) {
+				logger.error("회원탈퇴 실패 : {}", e);
+				resultMap.put("message", e.getMessage());
+				status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+		} else {
+			logger.error("사용 불가능 토큰!!!");
+			resultMap.put("message", FAIL);
+			status = HttpStatus.ACCEPTED;
+		}
+
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
 //	
 //	@ApiOperation(value = "이메일 인증코드 전송", notes = "전송한 인증코드를 반환한다.", response = Map.class)
 //	@PostMapping("/sendmail")
--- webterview_be/src/main/java/com/ssafy/webterview/util/DEConverter.java
+++ webterview_be/src/main/java/com/ssafy/webterview/util/DEConverter.java
@@ -6,6 +6,7 @@ import com.ssafy.webterview.dto.UserDto;
 import com.ssafy.webterview.entity.Board;
 import com.ssafy.webterview.entity.Comment;
 import com.ssafy.webterview.entity.User;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,7 @@ public class DEConverter {
     DEConverter(ModelMapper modelMapper){
         this.modelMapper = modelMapper;
 //        this.modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
     }
 
     private <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
