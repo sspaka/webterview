bdfe44a642873683650fbf5fb0e6b6a27519a8be
sspaka <sspaka@naver.com>
2022-07-27 17:46:38 +0900

    #17 css update

--- frontend/src/views/user/FindIdView.vue
+++ frontend/src/views/user/FindIdView.vue
@@ -2,46 +2,48 @@
 <img class="logo" src="@/assets/logo.png" alt="Logo" style="position: absolute; top: 20px; left:20px; width: 200px;" @click="logo">
 <div class="limiter d-flex justify-content-center">
   <div class="container-login100 shadow-lg">
-  <div class="card shadow-lg p-3 mb-5 bg-body rounded" style="width: 900px; margin-top: 7.500;margin-left: 7.500;margin-top: 75px;margin-left: 75px;margin-right: 75px;margin-bottom: 75px;">
-  <div class="head">
-    아이디 찾기
-  </div>
-  <div class="card-body col-lg-12">
-    <form @submit.prevent="findmail(credentials)">
-      <!-- <div>
-        <label class="col-lg-4 col-form-label" for="val-username">Username <span class="text-danger">*</span></label>
-        <input class="btn" id="UserNameyn" v-model="credentials.name" type="text" placeholder="이름을 입력하세요" required>
-      </div> -->
+    <div class="wrap-login100">
+      <div class="card shadow-lg p-3 mb-5 bg-body rounded" style="width: 900px; margin-top: 7.500;margin-left: 7.500;margin-top: 75px;margin-left: 75px;margin-right: 75px;margin-bottom: 75px;">
+      <div class="head">
+        아이디 찾기
+      </div>
+      <div class="card-body col-lg-12">
+        <form @submit.prevent="findmail(credentials)">
+          <!-- <div>
+            <label class="col-lg-4 col-form-label" for="val-username">Username <span class="text-danger">*</span></label>
+            <input class="btn" id="UserNameyn" v-model="credentials.name" type="text" placeholder="이름을 입력하세요" required>
+          </div> -->
 
-      <!-- name -->
-      <div class="form-group row">
-          <label class="col-lg-4 col-form-label" for="val-username">Username <span class="text-danger">*</span></label>
-          <div class="col-lg-6">
-              <input v-model="credentials.name" type="text"  class="form-control inputNew" id="val-username" name="val-username" placeholder="Enter a username.." required>
-              <span class="focus-input100"></span>
+          <!-- name -->
+          <div class="form-group row">
+              <label class="col-lg-4 col-form-label" for="val-username">Username <span class="text-danger">*</span></label>
+              <div class="col-lg-6">
+                  <input v-model="credentials.name" type="text"  class="form-control inputNew" id="val-username" name="val-username" placeholder="Enter a username.." required>
+                  <span class="focus-input100"></span>
+              </div>
           </div>
-      </div>
-      &nbsp;
-      
-      <!-- <div>
-        <input class="btn" id="UserPhoneyn" v-model="credentials.phone" type="tel" placeholder="010-0000-0000" required>
-      </div> -->
-      
-      <!-- phone -->
-      <div class="form-group row">
-          <label class="col-lg-4 col-form-label" for="val-phoneus">Phone (KR) <span class="text-danger">*</span></label>
-          <div class="col-lg-6">
-              <input v-model="credentials.phone" type="tel" class="form-control inputNew" id="val-phoneus" name="val-phoneus" placeholder="010-0000-0000" required>
+          &nbsp;
+          
+          <!-- <div>
+            <input class="btn" id="UserPhoneyn" v-model="credentials.phone" type="tel" placeholder="010-0000-0000" required>
+          </div> -->
+          
+          <!-- phone -->
+          <div class="form-group row">
+              <label class="col-lg-4 col-form-label" for="val-phoneus">Phone (KR) <span class="text-danger">*</span></label>
+              <div class="col-lg-6">
+                  <input v-model="credentials.phone" type="tel" class="form-control inputNew" id="val-phoneus" name="val-phoneus" placeholder="010-0000-0000" required>
+              </div>
           </div>
-      </div>
-      <div>
-      <button type="submit" class="btn btn" style="margin-top: 10px;  background-color: #30475E; color: white; border-radius: 25px;">Submit</button>
-      </div>
+          <div>
+          <button type="submit" class="btn btn" style="margin-top: 10px;  background-color: #30475E; color: white; border-radius: 25px;">Submit</button>
+          </div>
 
-    </form>
-    <div class="show-id" v-if="email.message==='success'">고객님의 아이디(이메일)은 <b style="color: red;">{{ email.userEmail }}</b> 입니다.</div>
-    </div>
-    <router-link to="/" ><p class="return" @click="erase">이전</p></router-link>
+        </form>
+        <div class="show-id" v-if="email.message==='success'">고객님의 아이디(이메일)은 <b style="color: red;">{{ email.userEmail }}</b> 입니다.</div>
+        </div>
+        <router-link to="/" ><p class="return" @click="erase">이전</p></router-link>
+        </div>
     </div>
   </div>
   </div>
