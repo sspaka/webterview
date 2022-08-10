a3119d7b9b6bc97e7e83d88951256ddcf66f2c20
전윤희 <eodhlwjr97@gmail.com>
2022-08-10 16:11:47 +0900

    #26 feat Service 파일구조 맞춰서 수정

--- webterview_be/src/main/java/com/ssafy/webterview/service/SmsService.java
+++ webterview_be/src/main/java/com/ssafy/webterview/service/SmsService.java
@@ -1,92 +1,14 @@
 package com.ssafy.webterview.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.webterview.dto.MessagesDto;
-import com.ssafy.webterview.dto.SmsRequestDto;
-import com.ssafy.webterview.dto.SmsResponseDto;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
-//import lombok.Value;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ssafy.webterview.dto.SmsResponseDto;
 
-@Service
-@Transactional
-public class SmsService {
-
-    private String serviceId = "ncp:sms:kr:290257082169:webterview";
-    private String accessKey = "rn1KeYzS2PQM2f9LfWG5";
-    private String secretKey = "D1tag8HompOLhFlNTIpZYsrikg8vtvEF2Ap2ssjo";
-
-
-    public SmsResponseDto sendSms(String recipientPhoneNumber, String content) throws JsonProcessingException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, URISyntaxException {
-        Long time = System.currentTimeMillis();
-        List<MessagesDto> messages = new ArrayList<>();
-        messages.add(new MessagesDto(recipientPhoneNumber, content));
-
-        SmsRequestDto smsRequestDto = new SmsRequestDto("SMS", "COMM", "82", "01027468729", "[webterview_web발신]", messages);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonBody = objectMapper.writeValueAsString(smsRequestDto);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("x-ncp-apigw-timestamp", time.toString());
-        headers.set("x-ncp-iam-access-key", this.accessKey);
-        String sig = makeSignature(time); //암호화
-        headers.set("x-ncp-apigw-signature-v2", sig);
-
-        HttpEntity<String> body = new HttpEntity<>(jsonBody,headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        SmsResponseDto smsResponseDto = restTemplate.postForObject(new URI("https://sens.apigw.ntruss.com/sms/v2/services/"+this.serviceId+"/messages"), body, SmsResponseDto.class);
-
-        return smsResponseDto;
-
-    }
-    public String makeSignature(Long time) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-
-        String space = " ";
-        String newLine = "\n";
-        String method = "POST";
-        String url = "/sms/v2/services/"+ this.serviceId+"/messages";
-        String timestamp = time.toString();
-        String accessKey = this.accessKey;
-        String secretKey = this.secretKey;
-
-        String message = new StringBuilder()
-                .append(method)
-                .append(space)
-                .append(url)
-                .append(newLine)
-                .append(timestamp)
-                .append(newLine)
-                .append(accessKey)
-                .toString();
-
-        SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "HmacSHA256");
-        Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(signingKey);
-
-        byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
-        String encodeBase64String = Base64.encodeBase64String(rawHmac);
-
-        return encodeBase64String;
-    }
+public interface SmsService {
+	public SmsResponseDto sendSms(String recipientPhoneNumber, String content) throws JsonProcessingException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, URISyntaxException;
+	public String makeSignature(Long time) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException;
 }
