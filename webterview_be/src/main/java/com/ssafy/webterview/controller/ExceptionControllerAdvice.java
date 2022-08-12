74a0d02004587ee5a87e5e2090eca66826d968c7
heeejoo0518 <heeejoo0518@gmail.com>
2022-08-12 15:41:02 +0900

    #21 feat restcontroller변경

--- webterview_be/src/main/java/com/ssafy/webterview/controller/ExceptionControllerAdvice.java
+++ webterview_be/src/main/java/com/ssafy/webterview/controller/ExceptionControllerAdvice.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @ControllerAdvice
+@RestController
 public class ExceptionControllerAdvice {
 
 	private Logger logger = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
