942c6cac855bdc9bbe21d3465bfa336f6a03b7b3
hxxejx <uiu1442@naver.com>
2022-08-14 19:38:03 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,31 +1,31 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
 //   transpileDependencies: true,
 //   // publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 //   publicPath: ''
 // })
 
-// module.exports = {
-//   publicPath: '',
-//   devServer: {
-//         disableHostCheck: true
-//     }
-// };
-
-module.exports = defineConfig({
-  outputDir: "../src/main/resources/static",
-  indexPath: "../static/index.html",
-
+module.exports = {
+  publicPath: '',
   devServer: {
-      proxy: {
-          '/': {
-            //   target: 'https://i7c205.p.ssafy.io/api/',
-              target: 'http://localhost:3000/api/',
-              pathRewrite: { '^/': '' },
-              changeOrigin: true,
-              secure: false,
-              ws: false,
-          }
-      }
-  }
-});
+        disableHostCheck: true
+    }
+};
+
+// module.exports = defineConfig({
+//   outputDir: "../src/main/resources/static",
+//   indexPath: "../static/index.html",
+
+//   devServer: {
+//       proxy: {
+//           '/': {
+//               target: 'https://i7c205.p.ssafy.io/api/',
+//               // target: 'http://localhost:3000/api/',
+//               pathRewrite: { '^/': '' },
+//               changeOrigin: true,
+//               secure: false,
+//               ws: false,
+//           }
+//       }
+//   }
+// });
