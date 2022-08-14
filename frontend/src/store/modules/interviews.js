188f9b5c11782390073ffb7da36fbc52edfec374
홍성준 <sspaka@naver.com>
2022-08-14 19:48:37 +0900

    interview

--- frontend/src/store/modules/interviews.js
+++ frontend/src/store/modules/interviews.js
@@ -2,7 +2,7 @@
 // import drf from '@/api/drf'
 import router from '@/router'
 import axios from 'axios'
-// import drf from '@/api/drf'
+import drf from '@/api/drf'
 
 
 // import _ from 'lodash'
@@ -47,8 +47,8 @@ export default {
     async fetchRoomList({dispatch, getters}, groupNo) {
       console.log(groupNo)
       await axios({
-          // url: drf.admins.listRoom(groupNo),
-          url: '/admin'+'/roomList/' + groupNo,
+          url: drf.admins.listRoom(groupNo),
+          // url: '/admin'+'/roomList/' + groupNo,
           method: 'get',
           headers: {
             'access-token': getters.authHeader['access-token'],
@@ -78,8 +78,8 @@ export default {
       // console.log(credentials)
       
       await axios({
-        // url: drf.admins.createGroup(),
-        url: '/admin/createGroup',
+        url: drf.admins.createGroup(),
+        // url: '/admin/createGroup',
         method: 'post',
         headers: getters.authHeader,
         data: credentials,
@@ -105,8 +105,8 @@ export default {
     finishInterview({ dispatch,getters}, groupNo) {
       // console.log(groupNo)
       axios({
-        // url: drf.admins.deleteGroup(groupNo),
-        url: `/admin/${groupNo}`,
+        url: drf.admins.deleteGroup(groupNo),
+        // url: `/admin/${groupNo}`,
         method: 'delete',
         headers: getters.authHeader,
         
@@ -123,8 +123,8 @@ export default {
     async createRooms( { getters }, room ) {
       console.log(room)
       await axios({
-        // url:drf.admins.createRoon(),
-        url:'/admin/createRoom',
+        url:drf.admins.createRoon(),
+        // url:'/admin/createRoom',
         method: 'post',
         headers: getters.authHeader,
         data: room,
@@ -136,8 +136,8 @@ export default {
     // 방 한개 추가하기
     async addRoom({ dispatch, getters }, ) {
       await axios({
-        // url:drf.admins.createRoom(),
-        url:'/admin/createRoom',
+        url:drf.admins.createRoom(),
+        // url:'/admin/createRoom',
         method: 'post',
         headers: getters.authHeader,
         data: {"num": 1, "groupNo": getters.groupNo},
@@ -150,8 +150,8 @@ export default {
     // 방 한개 삭제하기
     deleteRoom({ dispatch, getters }, roomNo) {
       axios({
-        // url:drf.admins.deleteRoom(roomNo),
-        url:`/admin/room/` + roomNo,
+        url:drf.admins.deleteRoom(roomNo),
+        // url:`/admin/room/` + roomNo,
         method: 'delete',
         headers: getters.authHeader,
       })
@@ -163,8 +163,8 @@ export default {
     
     async readGroup({getters}, userNo) {
       await axios({
-          // url: drf.admin.applicants(userNo),
-          url: '/admin'+'/group/' + userNo,
+          url: drf.admin.applicants(userNo),
+          // url: '/admin'+'/group/' + userNo,
           method: 'get',
           headers: {
             'access-token': getters.authHeader,
