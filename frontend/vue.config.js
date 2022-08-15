4d580b0fe0ceada44df82c9897a8c3e71d720fae
전윤희 <eodhlwjr97@gmail.com>
2022-08-15 14:14:36 +0900

    # fix contextpath 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,7 +6,7 @@ const { defineConfig } = require('@vue/cli-service')
 // })
 
 module.exports = defineConfig({
-    publicPath: '',
+    publicPath: '/fetest',
     devServer: {
 
             disableHostCheck: true
