01c3441cff1498f103892a1e1306fea2ac93eb8b
leeseongchan <gotls0001@naver.com>
2022-08-15 16:14:21 +0900

    # fix drf

--- frontend/src/views/main/sidebar/QnaSide.vue
+++ frontend/src/views/main/sidebar/QnaSide.vue
@@ -4,38 +4,52 @@
       <div class="wrap-login100" style="margin-left: 20%; margin-right: 10%;">
         <input class="writeBtn dohyeon" type="button" value="글쓰기" @click="boardwrite"/>
 
-        <div class="headLine2 mb-2">QnA</div>
-        <br>
+        <div class="headLine2 mb-2">공지 밎 Q&A</div>
         <table class="noto table" style="font-size: 16px">
-          <thead style="background-color: #f5f5f5; color: #111">
+          <thead style="background-color: #30475e; color: #fff">
             <tr>
-              <th>번호</th>
+              <th >번호</th>
               <th>제목</th>
-              <th>작성자</th>
-              <th>등록날짜</th>
+              <th >등록날짜</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="notice in notices" @click="goBoardDetail(notice.boardNo)" :key="notice.boardNo">
-              <td><div style="height: 60%; width: 60%; background-color: #f05454; color: #fff; border-radius: 15%; margin: auto;">공지</div> </td>
+              <td><div style="height: 60%; width: 60%; background-color: #3c90e2; color: #fff; border-radius: 15%; margin: auto;">공지</div> </td>
               <td>{{ notice.boardTitle }}</td>
-              <td>운영자</td>
-              <td>{{ changeDate(notice.boardRegdate) }}</td>
+              <td>{{ notice.boardRegdate }}</td>
             </tr>
-            <tr v-for="board in boards.content" @click="goBoardDetail(board.boardNo)" :key="board.boardNo" >
+            <tr v-for="board in boards" @click="goBoardDetail(board.boardNo)" :key="board.boardNo" >
               <td>{{ board.boardNo }}</td>
-              <td >{{ board.boardTitle }} <small>({{ board.commentCnt}})</small></td>
-              <td>{{board.userName}}</td>
-              <td>{{ changeDate(board.boardRegdate) }}</td>
+              <td >{{ board.boardTitle }}</td>
+              <td>{{ board.boardRegdate }}</td>
             </tr>
           </tbody>
         </table>
+
         <nav aria-label="...">
           <ul class="pagination pagination-sm">
-            <li v-for="page in pages" class="page-item" :id="page" :key="page"><a class="page-link" href="#" @click="goPage(page), changeActive(page)" >{{page+1}}</a></li>
+            <li class="page-item"><a class="page-link" href="#" @click="goPage(0)" >1</a></li>
+            <li class="page-item"><a class="page-link" href="#" @click="goPage(1)" >2</a></li>
+            <li class="page-item"><a class="page-link" href="#" @click="goPage(2)" >3</a></li>
           </ul>
         </nav>
-
+        <!-- <ul class="board-ul">
+          <li v-for="board in boards" :key="board.boardNo">
+            <div class="board-list d-flex align-items-center justify-content-center">
+              <div class="my-1 board">
+                <router-link :to="{ name: 'board', params: {boardNo: board.boardNo} }">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ board.boardTitle }}</h5>
+                    <small>{{ board.boardRegDate }}</small>
+                  </div>
+                  <p class="mb-1">{{ board.boardContent }}</p>
+                  <small>댓글: {{ board.commentCnt }}</small>
+                </router-link>
+              </div>
+            </div>
+          </li>
+        </ul> -->
       </div>
     </div>
   </div>
@@ -52,21 +66,14 @@
       return {
         params: {
           page: 0,
-          size: 10,
+          size: 5,
           userNo: '',
           sort: 'boardRegdate,desc',
         },
       }
     },
     computed: {
-      ...mapGetters(['boards','notices', 'profile', 'userNo']),
-      pages() {
-        const lst = []
-        for(let i=0; i<this.boards.totalPages; i++) {
-          lst.push(i)
-        }
-        return lst;
-      }
+      ...mapGetters(['boards','notices', 'profile', 'userNo'])
     },
     methods: {
       ...mapActions(['fetchBoards']),
@@ -79,26 +86,11 @@
       goPage(n) {
         this.params.page = n
         this.fetchBoards(this.params)
-      },
-      changeDate(date) {
-        const moment = require('moment-timezone')
-        const time = moment(date).tz("Asia/Seoul").format('YYYY-MM-DD');
-        return time
-      },
-      changeActive(page) {
-        for(let i=0; i<this.boards.totalPages; i++) {
-          if (i == page) {
-            document.getElementById(page).classList.add('active')
-          } else {
-            document.getElementById(i).classList.remove('active')
-          }
-        }
       }
     },
     created() {
       this.params.userNo = this.userNo
       this.fetchBoards(this.params)
-      document.getElementById("0").classList.add('active')
     },
   }
 </script>
@@ -122,8 +114,7 @@
     text-decoration: none;
     font-weight: 600;
     transition: 0.25s;
-    background-color: #3c90e2;
-    /* background-color: #f05454; */
+    background-color: #f05454;
     color: #fff;
   }
 
