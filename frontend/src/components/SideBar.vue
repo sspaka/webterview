02df2bce6062ab3eff6acb5c05efc1e1515aa4c4
이성찬 <97578445+seongchanleelee@users.noreply.github.com>
2022-08-16 23:40:38 +0900

    #35 sidebar css

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -2,16 +2,27 @@
   <div class="icon-bar btn-group-toggle" data-toggle="buttons">
     <br>
     <img class="logo" src="@/assets/logo.png" alt="Logo" style="width: 90%;" @click="logo">
-    <router-link @click="clickTab" to="/webterview/meetingroom_man">미팅룸관리</router-link>
-    <router-link @click="clickTab" to="/webterview/applicant_man">지원자관리</router-link>
-    <router-link @click="clickTab" to="/webterview/boards">게시판</router-link>
-    <router-link @click="clickTab" to="/webterview/ranking">순위</router-link>
+    <div :class="{'light': noticemeet ==='light', 'dark': noticemeet==='dark'}">
+      <router-link class="thema-meet" @click="clickTabmeet" to="/webterview/meetingroom_man">미팅룸관리</router-link>
+    </div>
+    <div :class="{'light': noticeappli ==='light', 'dark': noticeappli==='dark'}">
+      <router-link class="thema-appli" @click="clickTabappli" to="/webterview/applicant_man">지원자관리</router-link>
+    </div>
+    <div :class="{'light': noticeeval ==='light', 'dark': noticeeval==='dark'}">
+      <router-link class="thema-eval" @click="clickTabeval" to="/webterview/boards">게시판</router-link>
+    </div>
+    <div :class="{'light': noticerank ==='light', 'dark': noticerank==='dark'}">
+      <router-link class="thema-rank" @click="clickTabrank" to="/webterview/ranking">순위</router-link>
+    </div>
+
     <div class="dropdown-center">
       <a href="#" class="d-flex justify-content-center align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
         User
       </a>
       <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser1" style="background-color: #1b3b5b">
-        <router-link @click="clickTab" :to="{ name: 'profile', params: { useremail } }">프로필</router-link>
+        <div :class="{'light': noticeprof ==='light', 'dark': noticeprof==='dark'}">
+          <router-link class="thema-prof" @click="clickTabprof" :to="{ name: 'profile', params: { useremail } }">프로필</router-link>
+        </div>
         <li><hr class="dropdown-divider"></li>
         <li @click="logout"><a href="">로그아웃</a></li>
       </ul>
@@ -28,7 +39,11 @@ export default {
     components: { },
     data() {
       return {
-        notice: true,
+        noticemeet: "dark",
+        noticeappli: "light",
+        noticeeval: "light",
+        noticerank: "light",
+        noticeprof: "light",
       }
     },
     computed: {
@@ -42,10 +57,46 @@ export default {
       logo() {
         router.push({ name: 'meetingroom_man' })
       },
+      clickTabmeet() {
+        if (this.noticemeet ==="light") {
+          this.noticemeet = "dark"
+        } else {
+          this.noticemeet = "light"
+        }
+      },
+      clickTabappli() {
+        if (this.noticeappli ==="light") {
+          this.noticeappli = "dark"
+        } else {
+          this.noticeappli = "light"
+        }
+      },
+      clickTabeval() {
+        if (this.noticeeval ==="light") {
+          this.noticeeval = "dark"
+        } else {
+          this.noticeeval = "light"
+        }
+      },
+      clickTabrank() {
+        if (this.noticerank ==="light") {
+          this.noticerank = "dark"
+        } else {
+          this.noticerank = "light"
+        }
+      },
+      clickTabprof() {
+        if (this.noticeprof ==="light") {
+          this.noticeprof = "dark"
+        } else {
+          this.noticeprof = "light"
+        }
+      }
+      
       
     },
     created() {
-      console.log(window.location.pathname)
+      
     },
 
 }
@@ -92,6 +143,31 @@ export default {
   letter-spacing: 0px;
   transform: scale(1.2);
   cursor: pointer;
+}
+
+.dark .thema-meet{
+    background:#444;
+    color:#F05454
+}
+
+.dark .thema-appli{
+    background:#444;
+    color:#F05454
+}
+
+.dark .thema-eval{
+    background:#444;
+    color:#F05454
+}
+
+.dark .thema-rank{
+    background:#444;
+    color:#F05454
+}
+
+.dark .thema-prof{
+    background:#444;
+    color:#F05454
 }
 
 </style>
