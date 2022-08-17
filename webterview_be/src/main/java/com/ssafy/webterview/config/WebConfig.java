886f613c9aff2a6bde2de42faa2fe3bdbe2da8db
heeejoo0518 <heeejoo0518@gmail.com>
2022-08-17 23:54:18 +0900

    #28 feat readGroup 토큰필요없게 뺌

--- webterview_be/src/main/java/com/ssafy/webterview/config/WebConfig.java
+++ webterview_be/src/main/java/com/ssafy/webterview/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private static final String[] EXCLUDE_PATHS = {"/user/**","/error/**","/swagger-resources/**","/swagger-ui/**","/v2/api-docs",
 			"/interview/confirm", "/interview/applicant/unique","/interview/applicant/room","/score/eval",
-			"/naverapi/sms","/score/save","/admin/decrypt","/interview/applicant/info","/interview/applicant/savefile","/interview/applicant/url/**"};
+			"/naverapi/sms","/score/save","/admin/decrypt","/interview/applicant/info",
+			"/interview/applicant/savefile","/interview/applicant/url/**","/admin/group/**"};
 
 	private JwtInterceptor jwtInterceptor;
 	@Autowired
