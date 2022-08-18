bbc09a77a5134240b411cabf595a227bd3e3d0d8
전윤희 <eodhlwjr97@gmail.com>
2022-08-19 01:31:54 +0900

    #31 feat file.files[0]

--- frontend/src/components/interview/ATempScreen.vue
+++ frontend/src/components/interview/ATempScreen.vue
@@ -412,7 +412,7 @@ export default {
       const file = new File([recordedBlob], filename);
       let fd = new FormData();
       fd.append("fname", filename);
-      fd.append("file", file);
+      fd.append("file", file.files[0]);
       console.log("fd: " + fd);
 
       axios({
