556707408ad36fdc12770fef927f29c568cec80a
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-08-18 11:33:06 +0900

    #30 404 fin

--- frontend/src/views/NotFound404.vue
+++ frontend/src/views/NotFound404.vue
@@ -1,7 +1,7 @@
 <template> 
-    <div style="width: 100%; padding-right: 0.75rem; padding-left: 0.75rem; margin-right: auto; margin-left: auto;">
-        <div style="text-align: center !important;">
-            <div class="error mx-auto" data-text="404" style="font-size: 20rem; color: #f05454">404</div>
+    <div class="container-fluid">
+        <div class="text-center">
+            <div class="error mx-auto noto" data-text="404" style="font-size: 20rem; color: #f05454">404</div>
             <p class="lead text-gray-800 mb-5">존재하지 않는 페이지입니다. 주소를 다시 확인하세요.</p>
             <!-- <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p> -->
             <router-link to="/" ><p>&larr; 메인페이지로</p></router-link>
