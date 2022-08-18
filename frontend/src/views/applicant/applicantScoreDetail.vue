16cab1ee16944dbbdb2dca4f396124ed5d3d3df0
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-08-18 09:38:11 +0900

    Update applicantScoreDetail.vue

--- frontend/src/views/applicant/applicantScoreDetail.vue
+++ frontend/src/views/applicant/applicantScoreDetail.vue
@@ -61,7 +61,6 @@
                                     <table class="table">
                                         <thead style="background-color: #f5f5f5; color: #111">
                                         <tr>
-                                            <th scope="col" style="text-align: center;">번호</th>
                                             <th scope="col">특이사항</th>
                                             <!-- <th scope="col">타입</th> -->
                                         </tr>
@@ -73,7 +72,7 @@
                                             <td>{{ score.type }}</td>
                                         </tr> -->
                                         <tr v-for="(text, index) in texts" :key="index">
-                                            <td>{{index+1}}</td>
+                        
                                             <td>{{ text }}</td>
                                         </tr>
                                         </tbody>
