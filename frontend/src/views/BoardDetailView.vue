c3954abcfae6f1113011538350d3de5f74a36f73
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-08-18 10:13:53 +0900

    Update BoardDetailView.vue

--- frontend/src/views/BoardDetailView.vue
+++ frontend/src/views/BoardDetailView.vue
@@ -41,7 +41,7 @@
                 </div>
               </span>
           </div>
-          <div class="datail-content mb20 headLine2" style="line-height: 2; text-align: left; font-size: 16px; min-height: 300px; letter-spacing: 2px; word-spacing: 2px;">{{ board.boardContent }}</div>
+          <textarea class="datail-content mb20 headLine2" v-model="content" style="line-height: 2; text-align: left; font-size: 16px; min-height: 300px; letter-spacing: 2px; word-spacing: 2px; width: 100%"></textarea>
         </div>
         <br>
 
@@ -67,6 +67,7 @@
     data() {
       return {
         boardNo: "",
+        content: "",
       }
     },
     computed: {
@@ -98,6 +99,7 @@
         this.fetchBoard(this.boardNo)
       }
       this.board.boardContent.split('\n').join('<br>');
+      this.content = this.board.boardContent
     }
   }
 </script>
